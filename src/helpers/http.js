05847b506c08775ee3c209f2b13e16564bddbfce
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-12 06:32:40 +0700

    add http helper for get auth token

---
+++ src/helpers/http.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const { REACT_APP_BACKEND_URL } = process.env
+
+const http = (token = false) => {
+  return axios.create({
+    baseURL: REACT_APP_BACKEND_URL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined
+    }
+  })
+}
+
+export default http
