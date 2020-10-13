ae11b18a85360962af50f3a243039d5f894c0670
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-13 09:24:46 +0700

    change component did update when customer success login redirect to the page that user come from

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,7 +35,7 @@ class Login extends Component {
   componentDidUpdate () {
     if (this.props.customerAuth.isLogin) {
       this.props.getCustomerProfile(this.props.customerAuth.token)
-      this.props.history.push('/')
+      this.props.history.goBack()
     }
   }
 
