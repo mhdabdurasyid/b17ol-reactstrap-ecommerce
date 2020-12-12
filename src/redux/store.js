bb79e08b925da5b281ac25dbaa46bc95c4802f5d
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-12-12 10:33:35 +0700

    add redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import rootReducer from './reducers'
+import logger from 'redux-logger'
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(promiseMiddleware)
+  applyMiddleware(promiseMiddleware, logger)
 )
 
 export default store
